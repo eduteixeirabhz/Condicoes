263b2a9713a90b8a859a31520ea4099341572164
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 14:02:58 -0300

    Add files via upload

---
+++ Aula010.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual seu nome: ')).upper().lower()
+if nome == 'gustavo':
+    print('Que nome lindo você tem')
+else:
+    print('Seu nome é tão normal!')
+print('Bom dia, {}'.format(nome))
